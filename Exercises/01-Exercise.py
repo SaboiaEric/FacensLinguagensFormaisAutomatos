59f246e0cab902c9949a0f5d2d795793463eef53
Eric Saboia <saboia.eric@gmail.com>
2018-02-24 17:46:16 -0300

    Finished first exercise

--- Exercises/01-Exercise.py
+++ Exercises/01-Exercise.py
@@ -1,24 +1,25 @@
-inicialLetra = 'S' 
+import re
+#inicialLetra = 'S' 
 alfabeto = ['a','b']
-conversoes = ['S','AA','-','A','aB','Ba','-','B','Bb','b']
+#conversoes = ['S','AA','-','A','aB','Ba','-','B','Bb','b']
 
 def pertenceLinguagem(palavra):
+  print("\nChecking if word there is in language")
   for char in palavra:
-    if(char not in alfabeto):
-      return False
-  
-  if(not verificaGramatica(palavra)):
-    return False
+    if(char not in alfabeto): return False
+
+  if(not verificaGramatica(palavra)): return False
     
   return True
     
 def verificaGramatica(palavra):
-  #implementHere
+  answer = re.search(r'^b*((ab+){2})$', palavra)
+  if answer: return True
   
   return False
 
-resultado = pertenceLinguagem('aa')
-if resultado:
-  print('Pertence')
-else:
-  print('Nao Pertence')
+
+word = input('Go!! Write the word: ')
+resultado = pertenceLinguagem(word)
+if resultado: print('Pertence')
+else: print('Nao Pertence')
