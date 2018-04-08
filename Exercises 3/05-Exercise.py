8e61cda24dc4bf0b79b463fcea142865de0178f5
Eric Saboia <saboia.eric@gmail.com>
2018-04-08 14:54:43 -0300

    Finished 3th exercises

--- Exercises 3/05-Exercise.py
+++ Exercises 3/05-Exercise.py
@@ -1,13 +1,14 @@
 import re
-## Serve para validar somente um IPv6 porque eu não entendi muito o enunciado :)
-def CheckTheIPv6Address(n):
-  answer = re.search(r'^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$', n)
-  if answer: return True
 
+def checkIfWordIsInLanguage(word):
+  answer = re.findall(r'[01]+[1$]', word)
+  print(answer)
+  if answer: return True
+  
   return False
 
-print('Format (**) *****-****')
-value = input('Go!! Write the value: ')
-result = CheckTheIPv6Address(value)
+
+word = input('Go!! Write the word: ')
+result = checkIfWordIsInLanguage(word)
 if result: print('Yes')
 else: print('No')
